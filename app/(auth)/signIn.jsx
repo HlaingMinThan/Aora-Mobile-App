6e50b6bfc96d386abcdf7cb35fb2f9459fdbd5a4
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-09-10 22:27:38 +0700

    reusable formfield component & sign in page

--- app/(auth)/signIn.jsx
+++ app/(auth)/signIn.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { ScrollView, Text } from 'react-native'
+import React, { useState } from 'react'
+import { Image, ScrollView, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants';
+import FormField from '@/components/FormField';
+import CustomButton from '@/components/CustomButton';
+import { Link } from 'expo-router';
 
 const SignIn = () => {
+    let [form, setForm] = useState({
+        email: "",
+        password: ""
+    })
+
+    let [isLoading, setIsLoading] = useState(false);
+
+    const submit = () => {
+        setIsLoading(true);
+    }
+
     return (
         <SafeAreaView className="bg-primary h-full" >
-            <ScrollView >
-                <Text className="text-white">hi</Text>
+            <ScrollView  >
+                <View className="w-full h-full justify-center px-4">
+                    <Image source={images.logo} className="w-[115px] h-[35px]" resizeMode='contain' />
+                    <Text className="text-2xl mt-10 font-semibold text-white">Login To Aora</Text>
+                    <FormField title="Email" value={form.email} onChangeText={e => setForm({ ...form, email: e })} otherStyles="mt-7" keyboardType="email" />
+                    <FormField title="Password" value={form.password} onChangeText={e => setForm({ ...form, password: e })} otherStyles="mt-7" keyboardType="password" />
+                    <CustomButton isLoading={isLoading} title="Sign In" onPress={submit} containerStyle="w-full mt-7" />
+                    <View className="mt-4 flex-row items-center justify-center">
+                        <Text className="text-gray-100 text-lg text-center">Don't Have An Account ?</Text>
+                        <Link href="/signup" className="text-lg font-psemibold text-secondary"> Sign Up</Link>
+                    </View>
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
---
+++ components/FormField.jsx
@@ -0,0 +1,30 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { icons } from '@/constants';
+
+const FormField = ({ title, value, onChangeText, otherStyles, keyboardType, placeHolder, ...props }) => {
+
+    let [showPw, setShowPw] = useState(false);
+
+    return (
+        <View className={`space-y-2 ${otherStyles}`}>
+            <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+            <View className="w-full h-16 border-2 border-black-200 bg-black-100 rounded-2xl focus:border-secondary">
+                <TextInput
+                    className="flex-1 text-white p-2 font-semibold text-base"
+                    value={value}
+                    placeholder={placeHolder}
+                    onChangeText={onChangeText}
+                    secureTextEntry={keyboardType === 'password' && !showPw ? true : false}
+                />
+                {keyboardType === "password" && (
+                    <TouchableOpacity className="absolute right-3 top-5" onPress={() => setShowPw(!showPw)}>
+                        <Image source={showPw ? icons.eyeHide : icons.eye} className="w-6 h-6" />
+                    </TouchableOpacity>
+                )}
+            </View>
+        </View>
+    )
+}
+
+export default FormField
