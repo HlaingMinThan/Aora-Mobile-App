282d5eef7cb7b116ffdaf3b140387748c8423fc9
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-09-10 19:16:48 +0700

    remove prettier config

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,17 +1,12 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import React from "react";
+import { Stack } from "expo-router";
 
 const _layout = () => {
     return (
         <Stack>
-            <Stack.Screen
-                name="index" options={{ headerShown: false }}
-            />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
         </Stack>
-    )
-}
+    );
+};
 
-export default _layout
-
-const styles = StyleSheet.create({})
+export default _layout;
