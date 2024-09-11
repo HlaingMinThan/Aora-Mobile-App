3cc4a9c4e39be71c12c56c13398822c0d8f76ff0
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-09-11 19:49:43 +0700

    using react animatable

--- components/TrendingItem.jsx
+++ components/TrendingItem.jsx
@@ -1,27 +1,44 @@
 import { View, TouchableOpacity, Image, Text } from 'react-native'
 import React, { useState } from 'react'
 import { icons } from '@/constants';
+import { View as AnimatableView } from 'react-native-animatable';
 
-const TrendingItem = ({ video: { title, thumbnail } }) => {
+const TrendingItem = ({ video: { id, title, thumbnail } }) => {
     let [play, setPlay] = useState(false);
-    return (
-        play ?
-            <Text className="text-white">Playing</Text> :
-            (
-                <View className="w-52  mr-4 ">
-                    <TouchableOpacity
-                        activeOpacity={0.8}
-                        onPress={() => setPlay(true)}
-                        className="relative justify-center items-center ">
-                        <Image
-                            source={{ uri: thumbnail }}
-                            className="w-full h-64  mt-3 opacity-80 rounded-3xl"
-                            resizeMode='cover'
-                        />
-                        <Image source={icons.play} className="w-12 h-12 absolute" resizeMode='contain' />
-                    </TouchableOpacity>
-                </View>
-            )
+    let [activeItem, setActiveItem] = useState(2);
+    const zoomIn = {
+        0: {
+            scale: 1
+        },
+        1: {
+            scale: 0.9
+        }
+    }
+    const zoomOut = {
+        0: {
+            scale: 0.9
+        },
+        1: {
+            scale: 1
+        }
+    }
+    return (play ?
+        <Text className="text-white">Playing</Text> :
+        <AnimatableView animation={id === activeItem ? zoomOut : zoomIn}>
+            <View className="w-52  mr-4" >
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    onPress={() => setPlay(true)}
+                    className="relative justify-center items-center ">
+                    <Image
+                        source={{ uri: thumbnail }}
+                        className="w-full h-64  mt-3 opacity-80 rounded-3xl"
+                        resizeMode='cover'
+                    />
+                    <Image source={icons.play} className="w-12 h-12 absolute" resizeMode='contain' />
+                </TouchableOpacity>
+            </View>
+        </AnimatableView>
     )
 }
 
